6efb7e9cfbbd1c5f961193a1e1e97b590ded5043
Erik <erikanderson.lm@gmail.com>
2024-01-30 13:13:30 -0300

    add useState in amount values

--- src/App.js
+++ src/App.js
@@ -11,11 +11,33 @@ export default function App(){
         data ? JSON.parse(data) : []
     );
     const [income, setIncome] = useState(0);
+    const [expense, setExpense] = useState(0);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        const amountExpense = transactionsList
+        .filter((item) => item.expense)
+        .map((transaction) => Number(transaction.amount));
+
+        const amountIncome = transactionsList
+        .filter((item) => ! item.expense)
+        .map((transaction) => Number(transaction.amount));
+
+        const expense = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+        const income = amountExpense.reduce((acc, cur) => acc + cur, 0).toFixed(2);
+
+        const total = Math.abs(income - expense).toFixed(2);
+
+        setIncome(`R$ ${income}`);
+        setExpense(`R$ ${expense}`);
+        setTotal(`${Number(income) < Number(expense) ? "-" : ""}R$ ${total} `);
+
+    }, [transactionsList])
 
     return(
         <>
         <Header />
-        <Resume />
+        <Resume income={income} expense={expense} total={total} />
         <Form />
         <Global />
         </>
