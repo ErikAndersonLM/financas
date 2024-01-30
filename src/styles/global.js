7e59d4666513e6ccb35597903082ef49b0076ef7
unknown <erikanderson.lm@gmail.com>
2024-01-30 12:44:04 -0300

    first-commit

---
+++ src/styles/global.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    background: #f2f2f2;
+    font-family: 'Poppins', Sans-Serif;
+  }
+`;
+ 
+export default GlobalStyle;
